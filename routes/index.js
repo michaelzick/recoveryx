45d9b7992d8b9e0280cc924217932ea02ac4183f
Michael Zick <michaelzick@gmail.com>
2016-06-26 03:32:51 +0000

    DATA: got work data rendering to index.jade

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+
+var work = [];
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Main' });
+  MongoClient.connect("mongodb://localhost:27017/michaelmulti", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+    }
+    db.collection("work", function(err, collection) {
+      collection.find().sort({order_num: 1}).toArray(function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          for (var i = 0; i < result.length; i++) {
+            work[i] = result[i];
+          }
+        }
+      });
+    });
+  });
+
+  res.render('index', {
+    title: 'Main',
+    work: work
+  });
 });
+
 router.get('/other', function(req, res) {
   res.render('other', { title: 'Other' });
 });
-
 
 module.exports = router;
