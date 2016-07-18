b4efa2e0dcaecb15e6f616711e1010b1fe927838
Michael Zick <michaelzick@gmail.com>
2016-07-18 04:18:42 +0000

    CARDS: added .main-card to circle cards; hiding for < 768

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -205,9 +205,9 @@ var Main = React.createClass({
           <AppBarDrawer/>
           <div className="hero"></div>
           <div className="circles-block">
-            <Paper className="surfboard-card" style={styles.circlePaper} zDepth={2} circle={true} />
-            <Paper className="cleanup-card" style={styles.circlePaper} zDepth={2} circle={true} />
-            <Paper className="twelve-card" style={styles.circlePaper} zDepth={2} circle={true} />
+            <Paper className="surfboard-card main-card" style={styles.circlePaper} zDepth={2} circle={true} />
+            <Paper className="cleanup-card main-card" style={styles.circlePaper} zDepth={2} circle={true} />
+            <Paper className="twelve-card main-card" style={styles.circlePaper} zDepth={2} circle={true} />
           </div>
 
           <div className="main-block"></div>
