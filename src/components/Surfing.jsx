30deed0be5079723b3116953ba68c37e96f7bac3
Michael Zick <michaelzick@gmail.com>
2016-10-13 00:55:00 +0000

    SPACING: spaced surfing.jsx to 4

--- src/components/Surfing.jsx
+++ src/components/Surfing.jsx
@@ -15,90 +15,90 @@ import Nav from '../../src/components/Nav.jsx';
 injectTapEventPlugin();
 
 const styles = {
-  container: {
-    textAlign: 'center'
-  },
-  mainInfoCard: {
-    position: 'absolute',
-    top: '440px',
-    width: '90%'
-  }
+    container: {
+        textAlign: 'center'
+    },
+    mainInfoCard: {
+        position: 'absolute',
+        top: '440px',
+        width: '90%'
+    }
 };
 
 const MainInfoCard = () => (
-  <Card style={styles.mainInfoCard}>
-    <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-    <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
-  </Card>
+    <Card style={styles.mainInfoCard}>
+      <CardTitle title="Card title" subtitle="Card subtitle" />
+        <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+        </CardActions>
+    </Card>
 );
 
 var Surfing = React.createClass({
-  state: {
-    open: false
-  },
+    state: {
+        open: false
+    },
 
-  handleRequestClose: function() {
-    this.setState({
-      open: false,
-    });
-  },
+    handleRequestClose: function() {
+        this.setState({
+            open: false,
+        });
+    },
 
-  getInitialState: function() {
-    return Store.get();
-  },
+    getInitialState: function() {
+        return Store.get();
+    },
 
-  handleChange: function() {
-    Actions.set(this.state.value);
-  },
+    handleChange: function() {
+        Actions.set(this.state.value);
+    },
 
-  render: function() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
+    render: function() {
+        const standardActions = (
+            <FlatButton
+                label="Ok"
+                primary={true}
+                onTouchTap={this.handleRequestClose}
+            />
+        );
+
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-      	<div style={styles.container}>
-          <Nav label="Home" url="/"/>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          	<div style={styles.container}>
+                <Nav label="Home" url="/"/>
 
-          <Dialog
-              className='dialog'
-              open={this.state.open}
-              title="Sports"
-              actions={standardActions}
-              onRequestClose={this.handleRequestClose}
-            >
-            {this.state.dialogText}
-          </Dialog>
+                <Dialog
+                    className='dialog'
+                    open={this.state.open}
+                    title="Sports"
+                    actions={standardActions}
+                    onRequestClose={this.handleRequestClose}>
+                    {this.state.dialogText}
+                </Dialog>
 
-          <div className="clear"></div>
+                <div className="clear"></div>
 
-          <div className="hero">
-            <div className="hero-inner">
-              <h1 className="hero-title">Surfing</h1>
+                <div className="hero">
+                    <div className="hero-inner">
+                        <h1 className="hero-title">Surfing</h1>
+                    </div>
+                </div>
+
+                <div className="main-info-wrapper">
+                    <MainInfoCard/>
+                </div>
+
+                <Footer/>
             </div>
-          </div>
-
-          <div className="main-info-wrapper">
-            <MainInfoCard/>
-          </div>
-
-          <Footer/>
-        </div>
-      </MuiThemeProvider>
-  )}
+        </MuiThemeProvider>
+    )}
 });
 
 module.exports = Surfing;
