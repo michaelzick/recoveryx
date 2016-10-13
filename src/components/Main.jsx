fd9e8b85e0b81e1e77feb26c9018c421ee15f806
Michael Zick <michaelzick@gmail.com>
2016-10-13 00:11:53 +0000

    SPACING: made main.jsx 4 spaces

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -28,136 +28,136 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 injectTapEventPlugin();
 
 const styles = {
-  container: {
-    textAlign: 'center'
-  },
-  card: {
-    width: 200,
-    float: 'left'
-  },
-  circlePaper: {
-    height: 200,
-    width: 200,
-    margin: 20,
-    textAlign: 'center',
-    display: 'inline-block',
-    cursor: 'pointer',
-  },
+    container: {
+        textAlign: 'center'
+    },
+    card: {
+        width: 200,
+        float: 'left'
+    },
+    circlePaper: {
+        height: 200,
+        width: 200,
+        margin: 20,
+        textAlign: 'center',
+        display: 'inline-block',
+        cursor: 'pointer',
+    },
 };
 
 const InfoCard = () => (
-  <Card>
-    <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-    <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
-  </Card>
+    <Card>
+        <CardTitle title="Card title" subtitle="Card subtitle" />
+        <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+        </CardActions>
+    </Card>
 );
 
 var Main = React.createClass({
-  state: {
-    open: false
-  },
+    state: {
+        open: false
+    },
 
-  handleRequestClose: function() {
-    this.setState({
-      open: false,
-    });
-  },
+    handleRequestClose: function() {
+        this.setState({
+            open: false,
+        });
+    },
 
-  getInitialState: function() {
-    return Store.get();
-  },
+    getInitialState: function() {
+        return Store.get();
+    },
 
-  handleChange: function() {
-    Actions.set(this.state.value);
-  },
+    handleChange: function() {
+        Actions.set(this.state.value);
+    },
 
-  handleTouchTap: function(text) {
-    this.setState({
-      open: true,
-      dialogText: text
-    });
-  },
+    handleTouchTap: function(text) {
+        this.setState({
+            open: true,
+            dialogText: text
+        });
+    },
 
-  render: function() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
+    render: function() {
+        const standardActions = (
+          <FlatButton
+            label="Ok"
+            primary={true}
+            onTouchTap={this.handleRequestClose}
+          />
+        );
+
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-      	<div style={styles.container}>
-          <Nav label="Surfing" url="/surfing"/>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <div style={styles.container}>
+                <Nav label="Surfing" url="/surfing"/>
 
-          <Dialog
-              className='dialog'
-              open={this.state.open}
-              title="Sports"
-              actions={standardActions}
-              onRequestClose={this.handleRequestClose}
-            >
-            {this.state.dialogText}
-          </Dialog>
+                <Dialog
+                    className='dialog'
+                    open={this.state.open}
+                    title="Sports"
+                    actions={standardActions}
+                    onRequestClose={this.handleRequestClose}>
+                    {this.state.dialogText}
+                </Dialog>
 
-          <div className="clear"></div>
+                <div className="clear"></div>
 
-          <div className="hero">
-            <div className="hero-inner">
-              <h1 className="hero-title">RecoveryX</h1>
+                <div className="hero">
+                    <div className="hero-inner">
+                        <h1 className="hero-title">RecoveryX</h1>
+                    </div>
+                </div>
+
+                <div className="circles-block" ref="circles">
+                    <div className="circle-block">
+                        <Paper
+                            className="surfboard-card main-card"
+                            onTouchTap={this.handleTouchTap.bind(null, 'blah')}
+                            style={styles.circlePaper} zDepth={2}
+                            circle={true}>
+                        </Paper>
+                        <div>Sports</div>
+                    </div>
+
+                    <div className="circle-block">
+                        <Paper
+                            className="cleanup-card main-card"
+                            onTouchTap={this.handleTouchTap.bind(null, 'blah')}
+                            style={styles.circlePaper} zDepth={2}
+                            circle={true}>
+                        </Paper>
+                        <div>Service</div>
+                    </div>
+
+                    <div className="circle-block">
+                        <Paper
+                            className="twelve-card main-card"
+                            onTouchTap={this.handleTouchTap.bind(null, 'blah')}
+                            style={styles.circlePaper} zDepth={2}
+                            circle={true}>
+                        </Paper>
+                        <div>Steps</div>
+                    </div>
+                </div>
+
+                <div className="main-info-wrapper card-margin-top">
+                    <InfoCard/>
+                </div>
+
+                <Footer/>
             </div>
-          </div>
-
-          <div className="circles-block" ref="circles">
-              <div className="circle-block">
-                <Paper
-                  className="surfboard-card main-card"
-                  onTouchTap={this.handleTouchTap.bind(null, 'blah')}
-                  style={styles.circlePaper} zDepth={2}
-                  circle={true}>
-                </Paper>
-                <div>Sports</div>
-              </div>
-
-              <div className="circle-block">
-                <Paper
-                  className="cleanup-card main-card"
-                  onTouchTap={this.handleTouchTap.bind(null, 'blah')}
-                  style={styles.circlePaper} zDepth={2}
-                  circle={true}>
-                </Paper>
-                <div>Service</div>
-              </div>
-
-              <div className="circle-block">
-                <Paper
-                  className="twelve-card main-card"
-                  onTouchTap={this.handleTouchTap.bind(null, 'blah')}
-                  style={styles.circlePaper} zDepth={2}
-                  circle={true}>
-                </Paper>
-                <div>Steps</div>
-              </div>
-          </div>
-
-          <div className="main-info-wrapper card-margin-top">
-            <InfoCard/>
-          </div>
-
-          <Footer/>
-        </div>
-      </MuiThemeProvider>
-  )}
+        </MuiThemeProvider>
+    )}
 });
 
 module.exports = Main;
