ef7490c9a236da5340a75002b98542802f180e46
Michael Zick <michaelzick@gmail.com>
2016-10-23 04:31:31 +0000

    LOGO: tweaked logo css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -147,7 +147,7 @@ export default class Nav extends React.Component {
                     iconClassNameLeft="app-bar-burger"
                     className="app-bar"
                     title={
-                        <div className="nav-logo">
+                        <div className="nav-logo relative">
                             <a href="/">
                                 <span className="nav-logo-r">R</span>
                                 <span className="nav-logo-x primary-cyan-text">x</span>
