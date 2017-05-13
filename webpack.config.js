a211b50777fbe85b2ca8693675fb4ce43c8eaa34
Michael Zick <michaelzick@gmail.com>
2017-05-13 01:56:35 +0000

    CONFIG: added process.env plugin for react prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ var webpack = require('webpack');
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-  __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
+  __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false')),
+  'process.env': {NODE_ENV: JSON.stringify('production')}
 });
 
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
